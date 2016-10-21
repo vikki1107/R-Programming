32cae9ee717f78215caef5da8f81c54434460725
vikki1107 <vikki.1107@gmail.com>
2016-10-21 16:35:14 -0700

    rename and remove redundant counters

--- STAT6260.R
+++ STAT6260.R
@@ -36,31 +36,27 @@ for (i in 1:n)
   }
 }
 
-z
-
 bin.data1 <- function(x,bins)
-{ # start function bin.data
+{ # start function bin.data1
   # Below lines are to check if the bins are in ascending order
   if ( !(all(diff(bins) >= 0 ) ) ) { stop("bins vector is not in ascending order") }
   
   n = length(bins) + 1                           # length of the output vector
   z = numeric(n)                                 # output vector with n 0's
   bins = c(-Inf, bins, Inf)                      # append -Inf and Inf to bins to get the intervals for comparison
-  i = 1                                          # Initialize counter for input vector                                 
   
   for(i in x)                                  
   { # start for loop
-    j = 1                                        # initialize counter for the bin vector
     for (j in 1:n)
     { # start for loop for bins
-      if ((bins[j] < i) && (i <= bins[j+1])) # check if the first element of input vector falls in first bin 
+      if ((bins[j] < i) && (i <= bins[j+1]))     # check if the first element of input vector falls in first bin 
       {                                          # boundary. If so then increment the value of that index by 1
         z[j] = z[j] + 1                          # If not then go to the second bin boundary and so on until all the bins  
       }                                          # are checked. This is linear search which will take a bit time. 
     } # end for loop for bins
   } # end for loop
   return(z)                                        # return the output
-} # end function bin.data
+} # end function bin.data1
 
 
 # max sort
