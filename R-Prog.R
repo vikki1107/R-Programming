8769c6f8169f9e95839901d94e1cf50dc38e26e3
vikki1107 <vikki.1107@gmail.com>
2016-10-21 17:18:16 -0700

    Removed the redundant code

--- R-Prog.R
+++ R-Prog.R
@@ -38,39 +38,4 @@ for (i in 1:n)
   }
 }
 
-bin.data1 <- function(x,bins)
-{ # start function bin.data1
-  # Below lines are to check if the bins are in ascending order
-  if ( !(all(diff(bins) >= 0 ) ) ) { stop("bins vector is not in ascending order") }
-  
-  n = length(bins) + 1                           # length of the output vector
-  z = numeric(n)                                 # output vector with n 0's
-  bins = c(-Inf, bins, Inf)                      # append -Inf and Inf to bins to get the intervals for comparison
-  
-  for(i in x)                                  
-  { # start for loop
-    for (j in 1:n)
-    { # start for loop for bins
-      if ((bins[j] < i) && (i <= bins[j+1]))     # check if the first element of input vector falls in first bin 
-      {                                          # boundary. If so then increment the value of that index by 1
-        z[j] = z[j] + 1                          # If not then go to the second bin boundary and so on until all the bins  
-      }                                          # are checked. This is linear search which will take a bit time. 
-    } # end for loop for bins
-  } # end for loop
-  return(z)                                        # return the output
-} # end function bin.data1
-
-
-# max sort
-max.sort <- function(x)
-{
-  n = length(x)
-  y = numeric(n)
-  for (i in n:1)
-  {
-    y[i] = max(x)
-    x = x[-which(x==y[i])]
-  }
-  return(y)
-}
 
